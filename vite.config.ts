fb17d0da23a0285409014a6ef834d6ebcaf9750e
Abyse.Eclis <wasmoonk1@gmail.com>
2026-09-09 11:08:45 +0700

    feat: initialize Tauri configuration and update Vite dev server port to 1422

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   clearScreen: false,
   server: {
-    port: 1420,
+    port: 1422,
     strictPort: true
   },
   envPrefix: ["VITE_", "TAURI_"],
